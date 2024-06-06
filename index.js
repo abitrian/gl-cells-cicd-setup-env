b2de75557bb92720893c611b022bf84441e5ab8b
abitrian <andresbitrian@gmail.com>
2024-06-06 10:59:24 +0200

    chore(): use exec on command execution

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const shell = require('shelljs');
 const winston = require('winston');
+const exec = require('node:child_process');
 
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'logfile.log' })
+    ]
 });
 
 try {
@@ -18,6 +22,7 @@ try {
     const repositoryGradle = core.getInput('repository-gradle');
     const artifactoryUser = core.getInput('artifactory-user');
     const artifactoryPass = core.getInput('artifactory-password');
+    let TOKEN = '';
 
     logger.info(`configureNpm :: ${configureNpm}`);
     logger.info(`repositoryNpm :: ${repositoryNpm}`);
@@ -27,12 +32,19 @@ try {
     if (configureNpm) {
         logger.info('Set up Artifactory registry');
         shell.exec(`npm config set registry https://artifactory.globaldevtools.bbva.com:443/artifactory/api/npm/${repositoryNpm};`);
+        shell.exec("cat ~/.npmrc");
 
         logger.info('Generate token for Artifactory');
-        shell.exec(`TOKEN=$(curl -s -u${{artifactoryUser}}:${{artifactoryPass}} https://artifactory.globaldevtools.bbva.com:443/artifactory/api/npm/auth --insecure | grep _auth)`);
+        /*shell.exec(`TOKEN=$(curl -s -u${{artifactoryUser}}:${{artifactoryPass}} https://artifactory.globaldevtools.bbva.com:443/artifactory/api/npm/auth --insecure | grep _auth)`);
+        shell.exec("cat ~/.npmrc");*/
+        exec(`curl -s -u${{artifactoryUser}}:${{artifactoryPass}} https://artifactory.globaldevtools.bbva.com:443/artifactory/api/npm/auth --insecure | grep _auth`, (error, stdout, stderr) => {
+            logger.info("STDOUT:", stdout, ", STDERR:", stderr);
+            TOKEN = stdout;
+        });
 
         logger.info('Store token for Artifactory');
-        shell.exec("echo //artifactory.globaldevtools.bbva.com/artifactory/api/npm/:$TOKEN >> ~/.npmrc");
+        shell.exec(`echo //artifactory.globaldevtools.bbva.com/artifactory/api/npm/:$TOKEN >> ~/.npmrc`);
+        shell.exec("cat ~/.npmrc");
     }
 
 } catch (error) {
