2df8dd30f97b410b4f05b46fc7150d3e52634a12
abitrian <andresbitrian@gmail.com>
2024-06-06 12:09:03 +0200

    chore(): rebuild dist

--- dist/index.js
+++ dist/index.js
@@ -51076,6 +51076,14 @@ module.exports = require("net");
 
 /***/ }),
 
+/***/ 7718:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:child_process");
+
+/***/ }),
+
 /***/ 5673:
 /***/ ((module) => {
 
@@ -52902,11 +52910,14 @@ const core = __nccwpck_require__(7510);
 const github = __nccwpck_require__(513);
 const shell = __nccwpck_require__(3132);
 const winston = __nccwpck_require__(6317);
+const exec = __nccwpck_require__(7718);
 
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
+    transports: [
+        new winston.transports.Console()
+    ]
 });
 
 try {
@@ -52914,18 +52925,33 @@ try {
     // `configure-npm` 'Flag to configure npm'
     const configureNpm = core.getInput('configure-npm');
     const repositoryNpm = core.getInput('repository-npm');
+    const repositoryBower = core.getInput('repository-bower');
+    const repositoryGradle = core.getInput('repository-gradle');
     const artifactoryUser = core.getInput('artifactory-user');
     const artifactoryPass = core.getInput('artifactory-password');
+    let TOKEN = '';
+
+    logger.info(`configureNpm :: ${configureNpm}`);
+    logger.info(`repositoryNpm :: ${repositoryNpm}`);
+    logger.info(`repositoryBower :: ${repositoryBower}`);
+    logger.info(`repositoryGradle :: ${repositoryGradle}`);
 
     if (configureNpm) {
-        logger.info('Set up Artifactory registry');
+        logger.info('Set up Artifactory registry :: ');
         shell.exec(`npm config set registry https://artifactory.globaldevtools.bbva.com:443/artifactory/api/npm/${repositoryNpm};`);
+        shell.exec("cat ~/.npmrc");
 
-        logger.info('Generate token for Artifactory');
-        shell.exec(`TOKEN=$(curl -s -u${{artifactoryUser}}:${{artifactoryPass}} https://artifactory.globaldevtools.bbva.com:443/artifactory/api/npm/auth --insecure | grep _auth)`);
+        logger.info('Generate token for Artifactory :: ');
+        /*shell.exec(`TOKEN=$(curl -s -u${{artifactoryUser}}:${{artifactoryPass}} https://artifactory.globaldevtools.bbva.com:443/artifactory/api/npm/auth --insecure | grep _auth)`);
+        shell.exec("cat ~/.npmrc");*/
+        exec(`curl -s -u${{artifactoryUser}}:${{artifactoryPass}} https://artifactory.globaldevtools.bbva.com:443/artifactory/api/npm/auth --insecure | grep _auth`, (error, stdout, stderr) => {
+            logger.info("STDOUT:", stdout, ", STDERR:", stderr);
+            TOKEN = stdout;
+        });
 
         logger.info('Store token for Artifactory');
-        shell.exec("echo //artifactory.globaldevtools.bbva.com/artifactory/api/npm/:$TOKEN >> ~/.npmrc");
+        shell.exec(`echo //artifactory.globaldevtools.bbva.com/artifactory/api/npm/:$TOKEN >> ~/.npmrc`);
+        shell.exec("cat ~/.npmrc");
     }
 
 } catch (error) {
